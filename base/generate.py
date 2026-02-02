b3cd661ca04a6cc7de0b65adf00e3611e71bbb35
scheibling <lars@scheibling.se>
2026-02-02 17:44:09 +0100

    Run first test build

---
+++ base/generate.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+dir = os.path.dirname(__file__)
+phpver = [f.path for f in os.scandir(os.path.join(dir, 'php')) if f.is_dir() and f.name.startswith('8.')]
+
+args = "-".join(sys.argv)
+
+if os.path.exists(os.path.join(dir, 'patched')):
+    print('Already patched')
+    exit(0)
+
+for folder in phpver:
+    alpver = [f.path for f in os.scandir(folder) if f.is_dir() and f.name.startswith('alpine')]
+    
+    for alp in alpver:
+        with open(os.path.join(alp, 'zts', 'Dockerfile'), 'r') as f:
+            data = f.read()
+        
+        data = data.replace('sqlite-dev', 'sqlite-dev jpeg-dev freetype-dev libwebp-dev icu-dev libpng-dev libzip-dev mariadb-dev krb5-dev')
+        data = data.replace('--disable-zend-signals', '--disable-zend-signals --enable-zend-max-execution-timers --with-pdo-mysql --with-mysqli --enable-bcmath --with-imap-ssl --with-freetype --with-jpeg --with-webp --with-zip --with-kerberos --enable-intl')
+
+        if "devcontaienr" in args:
+            data = data.replace(f'FROM alpine:{alp}', f'FROM mcr.microsoft.com/devcontainers/base:alpine-{alp}')
+        
+        with open(os.path.join(alp, 'zts', 'Dockerfile'), 'w') as f:
+            f.write(data)
+        
+        print(f'Patched {os.path.join(alp, "zts", "Dockerfile")}')
+
+with open(os.path.join(dir, 'patched'), 'w') as f:
+    f.write('Patched\n')
